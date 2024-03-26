a937b7ac432b9b1421da371663983521f16b1163
Trebuh2006 <hubertborkowicz2006@gmail.com>
2024-03-26 14:22:49 +0100

    second commit

--- src/main/java/me/trbh/darkauction/npc/SpawnSirius.java
+++ src/main/java/me/trbh/darkauction/npc/SpawnSirius.java
@@ -11,6 +11,7 @@ import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.time.LocalTime;
 import java.util.HashSet;
@@ -28,30 +29,42 @@ public class SpawnSirius implements Listener {
             DarkAuction.getPlugin().getConfig().getDouble("sirius-location-z"),
             0, 0);
     private static Set<Player> signedInPlayers = new HashSet<>();
+    private static NPC.Global npcSirius;
+    private static boolean isSiriusVisible = false;
 
     public static void checkHour(){
 
         Bukkit.getScheduler().runTaskLater(DarkAuction.getPlugin(), () -> {
 
             NPCLib.getInstance().registerPlugin(DarkAuction.getPlugin());
-            NPC.Global npcSirius = NPCLib.getInstance().generateGlobalNPC(DarkAuction.getPlugin(), "siriusMain", siriusOverWorldLocation);
+            npcSirius = NPCLib.getInstance().generateGlobalNPC(DarkAuction.getPlugin(), "siriusMain", siriusOverWorldLocation);
             npcSirius.setText(ChatColor.GOLD + "Sirius", ChatColor.YELLOW + "" + ChatColor.BOLD + "CLICK");
             npcSirius.setGazeTrackingType(NPC.GazeTrackingType.NEAREST_PLAYER);
             npcSirius.setSkin("ewogICJ0aW1lc3RhbXAiIDogMTYxMjI5MDEyMjIxNSwKICAicHJvZmlsZUlkIiA6ICJkZGVkNTZlMWVmOGI0MGZlOGFkMTYyOTIwZjdhZWNkYSIsCiAgInByb2ZpbGVOYW1lIiA6ICJEaXNjb3JkQXBwIiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzY3YWUzZTI1MzlhNDQyNTE4MTMwMzcwZDNiMmQzYjE4MzdhNDkxZWUyM2Q1YWZmMjBmMDE1YWUyODRhMjhjOTAiLAogICAgICAibWV0YWRhdGEiIDogewogICAgICAgICJtb2RlbCIgOiAic2xpbSIKICAgICAgfQogICAgfQogIH0KfQ==",
                     "wxO8NN0XVV66aolUjL9qhturPPfas7lvoxTGCZ9jkV+ojvREtBS2fYuFTNaF5kB+SZdnZLphOrLBAHlyMIsGS7o+MhBmtlQblJBafdNHN5CsA5LpsbaHwj+WZjqSnzCV2qVVj2gY7w7HLlLV1PEPg97TcRS/SjIZND08257kJOkrCRxJBLGfQ8ZhQWl0p4JF5S4bn+kP754L1fyUx0kYWGstIZGmMcyQkY0aOI251bRapVa3rs+rc0QtOkRyy38OputzTaCREQ3m3fpCZ5lxG0IvR4NScXc3V1EJKwvsEpXUYsy1ElrrbYle+2sZvHdPS4JGYN8RLcVBiOHcncZ2xSSlep2O9rui/jtiTZAIvUGQX6Fn4RSRbeI9qinOr8qhGOnKiPsBXx18BNgCRNcj1rCmwAqmHGbP0eMZ/KYMoVlHZWlXBd1DMN/7duEoy5MV59+GK2sOO2Qkz1C+TeTKRV2gLe2tkTJMukUYS58m4BBZPavdY8RZAnzn0EP6pwWTrb1ms9ZA+CovfFTdGLh4fGuHjrhQBvdldQf00gbbDGcWnOoiiEfsZ/azW1VmB08XpIvRlLDTTgjmI1avwrd0BMe+oRNt9wOLFnUbPZ+weIKPTT/iyrhthG3dialYr+GgTJGJ/+QHKJY9gWkDIcDrl3Iei3Va1tuI2VvEtf9wcy8=");
 
+            Bukkit.getScheduler().runTaskLater(DarkAuction.getPlugin(), () -> {
+
+                isSiriusVisible = false;
+                npcSirius.hide();
+
+            }, 20L);
+
             Bukkit.getScheduler().scheduleSyncRepeatingTask(DarkAuction.getPlugin(), () -> {
 
                 LocalTime currentTime = LocalTime.now();
                 int currentHour = currentTime.getHour();
+                int currentMinute = currentTime.getMinute();
 
-                if (currentHour % 2 == 0) {
+                if (currentHour % 2 == 0 && currentMinute == 0) {
 
+                    isSiriusVisible = true;
                     npcSirius.show();
                     Bukkit.getScheduler().runTaskLater(DarkAuction.getPlugin(), () -> npcSirius.getWorld().spawnParticle(Particle.SQUID_INK, npcSirius.getLocation(), 50), 15L);
 
                     Bukkit.getScheduler().runTaskLater(DarkAuction.getPlugin(), () -> {
 
+                        isSiriusVisible = false;
                         npcSirius.hide();
 
                         if(signedInPlayers.size() >= DarkAuction.getPlugin().getConfig().getDouble("min-signed-in-players")){
@@ -79,7 +92,7 @@ public class SpawnSirius implements Listener {
 
                 }
 
-            }, 0, 30 * 20L);
+            }, 0, 60 * 20L);
 
         }, 20L);
 
@@ -90,7 +103,7 @@ public class SpawnSirius implements Listener {
         Player p = e.getPlayer();
         NPC npc = e.getNPC();
 
-        if(npc.getSimpleID().equalsIgnoreCase("global_siriusmain")){
+        if(npc.getSimpleID().equalsIgnoreCase("global_siriusmain") && isSiriusVisible){
 
             if(!signedInPlayers.contains(p)){
                 p.sendMessage(siriusVoice("Are you here for the Auction? Only the richest players can enter."));
@@ -102,6 +115,14 @@ public class SpawnSirius implements Listener {
                 p.sendMessage(siriusVoice("You are signed up for the Auction!"));
                 playSiriusSound(p);
             }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e){
+
+        if(!isSiriusVisible){
+            Bukkit.getScheduler().runTaskLater(DarkAuction.getPlugin(), () -> npcSirius.hide(), 20L);
         }
     }
 }
